0bf91f24457ab29720093ebc3076dc1237cc1fd6
SamGriffin909 <87524279+SamGriffin909@users.noreply.github.com>
2021-08-26 22:40:00 +0100

    updated weather icon

--- src/index.js
+++ src/index.js
@@ -35,15 +35,20 @@ let descriptionElement = document.querySelector("#city-description")
 let humidityElement = document.querySelector("#city-humidity");
 let windElement = document.querySelector("#city-windspeed");
 let dateElement = document.querySelector("#city-date");
+let iconElement = document.querySelector("#icon");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
-descriptionElement.innerHTML = response.dat.weather[0].description;
-humidityElement.innerHTML = response.data.main.humidity;
-windElement.innerHTML = Math.round(response.data.wind.speed);
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = `Humidity: ${response.data.main.humidity} %`;
+windElement.innerHTML = `WindSpeend: ${Math.round(response.data.wind.speed)}`;
 dateElement.innerHTML = formateDate(response.data.dt*1000);
+iconElement.setAttribute("src,"`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt,"`http://openweathermap.org/img/wn/${response.dat.weather[0].description}@2x.png`);
 }
 
+let city = "New York"
 let apikey = "5efeb759a9a2d75776085db8550371e8";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apikey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+
 
 axios.get(apiUrl).then (displayTemperature);
